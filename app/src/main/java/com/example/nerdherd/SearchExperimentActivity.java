ae718caf0e664d418caebc2af9c23131086e9e73
zhipeng4 <zhipeng4@ualberta.ca>
2021-03-03 12:15:16 -0700

    Small change

--- app/src/main/java/com/example/nerdherd/SearchExperimentActivity.java
+++ app/src/main/java/com/example/nerdherd/SearchExperimentActivity.java
@@ -2,22 +2,26 @@ package com.example.nerdherd;
 
 // Author: Zhipeng Z zhipeng4
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class SearchExperimentActivity extends AppCompatActivity {
+public class SearchExperimentActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle actionBarDrawerToggle;
     private NavigationView navigationView;
     private Toolbar toolbar;
+    private Intent searchUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +38,14 @@ public class SearchExperimentActivity extends AppCompatActivity {
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
         actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.user_search){
+            searchUser = new Intent(SearchExperimentActivity.this, SearchUserActivity.class);
+            startActivity(searchUser);
+        }
+        return true;
     }
 }
