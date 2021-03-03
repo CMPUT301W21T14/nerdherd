bcfce24cc66d7cde29202e678ea2051a74d245c2
zhipeng4 <zhipeng4@ualberta.ca>
2021-03-02 23:30:00 -0700

    Add a menu

--- app/src/main/java/com/example/nerdherd/SearchExperimentActivity.java
+++ app/src/main/java/com/example/nerdherd/SearchExperimentActivity.java
@@ -2,15 +2,37 @@ package com.example.nerdherd;
 
 // Author: Zhipeng Z zhipeng4
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class SearchExperimentActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_experiment);
+
+        toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.draw_layout);
+        navigationView = findViewById(R.id.navigator);
+
+        setSupportActionBar(toolbar);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
     }
 }
