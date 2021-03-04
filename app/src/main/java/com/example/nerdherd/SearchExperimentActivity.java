a2cc0b11d58072bb3a817b3611c0c864583cdcd2
zhipeng4 <zhipeng4@ualberta.ca>
2021-03-03 18:51:25 -0700

    main UI functional
    need create experiment to test

--- app/src/main/java/com/example/nerdherd/SearchExperimentActivity.java
+++ app/src/main/java/com/example/nerdherd/SearchExperimentActivity.java
@@ -8,13 +8,19 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
 
 public class SearchExperimentActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
@@ -23,6 +29,8 @@ public class SearchExperimentActivity extends AppCompatActivity implements Navig
     private NavigationView navigationView;
     private Toolbar toolbar;
     private Intent searchUser;
+    private ExperimentAdapter.onClickListener listener;
+    private ArrayList<Experiment> experimentList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +49,23 @@ public class SearchExperimentActivity extends AppCompatActivity implements Navig
         actionBarDrawerToggle.syncState();
 
         navigationView.setNavigationItemSelectedListener(this);
+
+        RecyclerView recyclerView = findViewById(R.id.experiment_recyclerView);
+        listener = new ExperimentAdapter.onClickListener() {
+            @Override
+            public void onClick(View view, int index) {
+                Log.d("HaHaHa","Still in test");
+            }
+        };
+// Test below
+        experimentList = new ArrayList<Experiment>();
+        experimentList.add(new Experiment("owner", "status", "title"));
+// Test above
+        ExperimentAdapter adapter = new ExperimentAdapter(experimentList, listener);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
