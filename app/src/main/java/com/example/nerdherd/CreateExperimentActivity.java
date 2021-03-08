a16dbcecbc70dddf3b430427b961af80acdbca47
Andrew <adearbor@ualberta.ca>
2021-03-07 18:14:19 -0700

    skeleton for experiment creation

---
+++ app/src/main/java/com/example/nerdherd/CreateExperimentActivity.java
@@ -0,0 +1,14 @@
+package com.example.nerdherd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateExperimentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_experiment);
+    }
+}
