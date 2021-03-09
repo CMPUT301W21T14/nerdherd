9c021468637f9ae6e508fc9b9b861b5b85a395aa
ogosamuel <78457854+ogosamuel@users.noreply.github.com>
2021-03-09 15:04:53 -0700

    Update ProfileActivity.java
    added changes to differentiate current user from public user
    Menu controller implemented 
    Updated user information

--- app/src/main/java/com/example/nerdherd/ProfileActivity.java
+++ app/src/main/java/com/example/nerdherd/ProfileActivity.java
@@ -1,5 +1,6 @@
 package com.example.nerdherd;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -18,13 +20,18 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class ProfileActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+import java.util.ArrayList;
+
+public class ProfileActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle actionBarDrawerToggle;
     private NavigationView navigationView;
     private Toolbar toolbar;
     private Intent searchUser;
     private Intent UsersProfile;
+    private Intent Experiments;
+    private Intent Subscription;
+    private Intent myExperiments;
     private Bundle dataBundle;
     //the users custom image
     private ImageView usersAvatar;
@@ -36,7 +43,10 @@ public class ProfileActivity extends AppCompatActivity implements NavigationView
     public  String email;
     private String id;
     //profile controller
+    private ArrayList<String> emailData;
+    private Boolean isNew;
     private ProfileController profController;
+    private MenuController menuController;
     private FireStoreController fireStoreController;
     //set the users name
     private TextView usersname;
@@ -45,71 +55,66 @@ public class ProfileActivity extends AppCompatActivity implements NavigationView
     private Button edtUserProfile;
     private Intent publicuser;
     private int val;
+    private String useridval;
 
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.draw_layout);
+        navigationView = findViewById(R.id.navigator);
+
+        setSupportActionBar(toolbar);
+
+        menuController = new MenuController(ProfileActivity.this, toolbar, navigationView, drawerLayout);
+        menuController.useMenu();
+
+
         edtUserProfile = findViewById(R.id.edt_profile);
         name = GlobalVariable.profile.getName();
         email = GlobalVariable.profile.getEmail();
+        usersAvatar = findViewById(R.id.avatarEdit2);
         publicuser = getIntent();
         Bundle pUser = publicuser.getExtras();
-
+        profController= new ProfileController();
         //if pUser is null then it is a user(current logged in user or public user)
         // that needs to be displayed
         if (pUser != null)
         {
 
+            avatar = profController.getAvatar();
+            usersAvatar.setImageResource(profController.getImageArray().get(avatar));
+
+
             Intent intent = getIntent();
             val = intent.getIntExtra(SearchUserActivity.EXTRA_MESSAGE, -1);
             name = GlobalVariable.profileArrayList.get(val).getName();
             email = GlobalVariable.profileArrayList.get(val).getEmail();
+            avatar = GlobalVariable.profileArrayList.get(val).getAvatar();
+            id = GlobalVariable.profileArrayList.get(val).getId();
             //if the public name matches up with the private name
             //or if the searched user == the logged in user
             //then set the button to be visible (give functionality) to edit your profile
             // else remove the button
-            if (name.equals(GlobalVariable.profile.getName()))
+            if (id.equals(GlobalVariable.profile.getId()))
             {
                 edtUserProfile.setVisibility(View.VISIBLE);
+
             }
             else{
                 edtUserProfile.setVisibility(View.GONE);
+                usersAvatar.setImageResource(profController.getImageArray().get(avatar));
             }
 //            Log.d("MYINT", "publicname: "+ sp);
         }
 
-
-
-        //if no value is stored -> -1
-        //else: it returns the index
-//        GlobalVariable.profileArrayList.get(val).getName();
-//        if (pUser!= null)
-//        {
-//            publicName = pUser.getString("Index");
-//            Log.d("Userval", publicName);
-//        }
-//        String Name = GlobalVariable.profileArrayList.get(publicName).getName();
-
-
-//        avatar = 0;
-//        getAvatar = getIntent();
-//        dataBundle = getAvatar.getBundleExtra("Data");
-        //get the users name
-
         fireStoreController = new FireStoreController();
-//        if (dataBundle != null) {
-//            avatar = dataBundle.getInt("Index", 0);
-//            name = dataBundle.getString("Name");
-//            password = dataBundle.getString("Password");
-//            email = dataBundle.getString("Email");
-//        }
-        profController= new ProfileController();
         Log.d("Test", GlobalVariable.profile.getName());
         toolbar = findViewById(R.id.toolbar);
         drawerLayout = findViewById(R.id.draw_layout);
         navigationView = findViewById(R.id.navigator);
-        usersAvatar = findViewById(R.id.avatarEdit2);
+
         setSupportActionBar(toolbar);
 
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
@@ -128,68 +133,97 @@ public class ProfileActivity extends AppCompatActivity implements NavigationView
         uname.setText(name+"");
 
 
-        //TODO: needs to fix to implement controller as opposed to model(profile class)
-        avatar = GlobalVariable.profile.getAvatar();
-        navigationView.setNavigationItemSelectedListener(this);
-
+        avatar = profController.getAvatar();
+        usersAvatar.setImageResource(profController.getImageArray().get(GlobalVariable.profile.getAvatar()));
+        
         edtUserProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new EditProfileFragment().show(getSupportFragmentManager(), "EDIT_PROFILE");
+                new EditProfileFragment(avatar).show(getSupportFragmentManager(), "EDIT_TEXT2");
             }
         });
 
-//        usersImg.setImageResource(R.drawable.zelda);
-        usersAvatar.setImageResource(profController.getImageArray().get(avatar));
 
-        //set the users name
 
-//        usersAvatar.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                dataBundle = new Bundle();
-//                dataBundle.putString("Name", GlobalVariable.profile.getName());
-//                avatarPicker = new Intent(profileActivity.this, AvatarPicker.class);
-//                avatarPicker.putExtra("Data", dataBundle);
-//                startActivity(avatarPicker);
-//                finish();
-//            }
-//        });
     }
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.user_search){
-            searchUser = new Intent(ProfileActivity.this, SearchUserActivity.class);
-            startActivity(searchUser);
-            finish();
-        }
-        if (item.getItemId() == R.id.my_profile){
-            UsersProfile = new Intent(ProfileActivity.this, ProfileActivity.class);
-            startActivity(UsersProfile);
-            finish();
-        }
-        return true;
-    }
-
-    //This method responsible for interacting with controller which calls
-    //profile to get the users id, After changes made by the user (Edit their information)
-    //the firestoreController updates their information and returns 'success' or 'failure'
-    public void updateUserInfo(String Name, String Email){
-        String id = profController.getId();
-        fireStoreController.updater(id, "Name", Name, new FireStoreController.FireStoreUpdateCallback(){
+    public void updateEmail(String Email){
+        isNew = true;
+        useridval = profController.getId();
+        emailData = new ArrayList<String>();
+        fireStoreController.readData(emailData, "Email", new FireStoreController.FireStoreReadCallback() {
             @Override
-            public void onCallback() {
-                Toast.makeText(getApplicationContext(), " Successfully updated.", Toast.LENGTH_SHORT).show();
-                uname.setText(Name+"");
+            public void onCallback(ArrayList<String> list) {
+                for (String usedEmail : list) {
+                    if (usedEmail.equals(useridval)) {
+                        isNew = false;
+                    }
+                }
+                if (isNew) {
+                    fireStoreController.updater(useridval, "Email", Email, new FireStoreController.FireStoreUpdateCallback() {
+                        @Override
+                        public void onCallback() {
+                            usersemail.setText(Email+"");
+                        }
+                    }, new FireStoreController.FireStoreUpdateFailCallback() {
+                        @Override
+                        public void onCallback() {
+                            Toast.makeText(getApplicationContext(), "The database cannot be accessed at this point, please try again later. Thank you.", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } else {
+                    Toast.makeText(getApplicationContext(), "The email address is already used. Thank you.", Toast.LENGTH_SHORT).show();
+                }
             }
-        }, new FireStoreController.FireStoreUpdateFailCallback(){
+        }, new FireStoreController.FireStoreReadFailCallback() {
             @Override
             public void onCallback() {
                 Toast.makeText(getApplicationContext(), "The database cannot be accessed at this point, please try again later. Thank you.", Toast.LENGTH_SHORT).show();
             }
         });
     }
+
+
+
+
+    public void updateUserProfile(String Name, String Email, String Password) {
+        useridval = profController.getId();
+        if (!Email.isEmpty()){
+            updateEmail(Email);
+        }
+        if (!Name.isEmpty()){
+            fireStoreController.updater(useridval, "Name", Name, new FireStoreController.FireStoreUpdateCallback() {
+                @Override
+                public void onCallback() {
+                    uname.setText(Name+"");
+                    usersname.setText(Name+"");
+
+                }
+            }, new FireStoreController.FireStoreUpdateFailCallback() {
+                @Override
+                public void onCallback() {
+                    Toast.makeText(getApplicationContext(), "The database cannot be accessed at this point, please try again later. Thank you.", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        if (!Password.isEmpty()){
+            fireStoreController.updater(useridval, "Password", Password, new FireStoreController.FireStoreUpdateCallback() {
+                @Override
+                public void onCallback() {
+
+                }
+            }, new FireStoreController.FireStoreUpdateFailCallback() {
+                @Override
+                public void onCallback() {
+                    Toast.makeText(getApplicationContext(), "The database cannot be accessed at this point, please try again later. Thank you.", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        if (!Password.isEmpty() | !Name.isEmpty() | !Email.isEmpty()){
+            Toast.makeText(getApplicationContext(), "User Profile Updated", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
 
 
