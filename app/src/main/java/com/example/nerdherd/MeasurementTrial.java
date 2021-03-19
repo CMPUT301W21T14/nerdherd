038235192e2520001e53c7131c828d5845b9956a
utkarsh-saraswat <70922561+utkarsh-saraswat@users.noreply.github.com>
2021-03-18 23:33:36 -0600

    updated comments

--- app/src/main/java/com/example/nerdherd/MeasurementTrial.java
+++ app/src/main/java/com/example/nerdherd/MeasurementTrial.java
@@ -1,5 +1,9 @@
 package com.example.nerdherd;
 
+/* This class inherits the Trial.
+*Measurement trials are repeated measurements and you record a decimal value of whatever was measured.
+* */
+
 public class MeasurementTrial extends Trial {
 
     private int totalMeasurementCount;
