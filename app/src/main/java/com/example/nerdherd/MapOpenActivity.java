5508c37d04d3aca6f417a591f59092faf06a74a7
utkarsh-saraswat <70922561+utkarsh-saraswat@users.noreply.github.com>
2021-04-07 12:18:50 -0600

    updated MapActivity

--- app/src/main/java/com/example/nerdherd/MapOpenActivity.java
+++ app/src/main/java/com/example/nerdherd/MapOpenActivity.java
@@ -1,10 +1,31 @@
 package com.example.nerdherd;
 
+import android.location.Location;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
 public class MapOpenActivity extends AppCompatActivity {
+
+
+
+    private SupportMapFragment mapFragment;
+    private GoogleMap map;
+    private LocationRequest mLocationRequest;
+    Location mCurrentLocation;
+    private long UPDATE_INTERVAL = 60000;  /* 60 secs */
+    private long FASTEST_INTERVAL = 5000; /* 5 secs */
+
+    private final static String KEY_LOCATION = "location";
+
+    /* Define a request code to send to Google Play services This code is
+     * returned in Activity.onActivityResult*/
+    
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) { // getting the map ready
