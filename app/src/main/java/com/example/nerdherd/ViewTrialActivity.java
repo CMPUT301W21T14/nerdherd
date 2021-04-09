756ac2d52abf36f64da33f1adcb1b96f02c0a02a
gochibhai <75108288+gochibhai@users.noreply.github.com>
2021-04-09 13:35:02 -0600

    worked on show all locations, utkarsh working on gpsenabled

--- app/src/main/java/com/example/nerdherd/ViewTrialActivity.java
+++ app/src/main/java/com/example/nerdherd/ViewTrialActivity.java
@@ -8,6 +8,7 @@ import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -17,8 +18,23 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -100,6 +116,42 @@ public class ViewTrialActivity extends Activity {
     }
 
     private void showAllLocations() {
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference locationsCol = db.collection("Experiment").document(experimentTitle)
+                .collection("Locations");
+
+        locationsCol.get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+
+                                Map<String, Object> location = document.getData();
+                                Map<String, Double> latLngMap = (HashMap<String, Double>) location.get("latlng");
+
+                                LatLng latLng = new LatLng(latLngMap.get("latitude"),latLngMap.get("longitude"));
+
+                                // Creating a marker
+                                MarkerOptions markerOptions = new MarkerOptions();
+
+                                // Setting the position for the marker
+                                markerOptions.position(latLng);
+
+                                // Setting the title for the marker.
+                                // This will be displayed on taping the marker
+                                markerOptions.title(latLng.latitude + " : " + latLng.longitude);
+
+
+                                // Placing a marker
+                                mMap.addMarker(markerOptions);
+                            }
+                        } else {
+                            Log.d("TAG", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
     }
 
     void isGpsEnabled(Context context) {
