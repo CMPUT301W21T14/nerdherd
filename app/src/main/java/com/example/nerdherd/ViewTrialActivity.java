2a214f2b1640601f5635479bafa1ec638cbb0b22
utkarsh-saraswat <70922561+utkarsh-saraswat@users.noreply.github.com>
2021-04-09 13:24:01 -0600

    getting multiple markers ready
    will write methods

--- app/src/main/java/com/example/nerdherd/ViewTrialActivity.java
+++ app/src/main/java/com/example/nerdherd/ViewTrialActivity.java
@@ -1,6 +1,14 @@
 package com.example.nerdherd;
 
 import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
+
 
 /**
  * Multiple location shower
@@ -9,5 +17,50 @@ import android.app.Activity;
  */
 
 public class ViewTrialActivity extends Activity {
+
+    private SupportMapFragment smf;
+    private FusedLocationProviderClient client;
+    private String experimentTitle;
+    private GoogleMap mMap;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_trial);
+
+        experimentTitle = getIntent().getStringExtra("expTitle");
+
+        isGpsEnabled(ViewTrialActivity.this);
+
+        smf = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+        Dexter.withContext(getApplicationContext())
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        getmylocation();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
+
+    }
+
+    private void getmylocation() {
+    }
+
+    void isGpsEnabled(Context context) {
+    }
 
 }
